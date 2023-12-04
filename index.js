8ad26feebe73341d4dbdf2834ac03d79f971268f
Julia Bodian <julia.h.bodian@gmail.com>
2023-12-04 17:24:32 -0500

    attempt http call with nook to weather api

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const URL = `https://api.weather.gov/gridpoints/ALY/110,79/forecast`;
+const URL = `http://api.weather.gov/gridpoints/ALY/110,79/forecast`;
 
 fetch(URL)
   .then(response => response.json())
   .then(data => {
     console.log(data.properties);
-    let forecast = data.properties.periods[0];
-    const { name, temperature, windSpeed, windDirection, shortForecast, detailedForecast } = forecast;
+    const currentForecast = data.properties.periods[0];
+    const { name, temperature, windSpeed, shortForecast, detailedForecast } = currentForecast;
+
+    const laterForecast = data.properties.periods[1];
+
     const weather = document.querySelector('#forecast');
     weather.innerHTML = `
       <h3>${name}</h3>
-      <p>${temperature} ${windSpeed} ${windDirection}</p>
+      <p>${temperature}°F Wind: ${windSpeed}</p>
       <p>${shortForecast}</p>
       <p>${detailedForecast}</p>
+      <h3>${laterForecast.name}</h3>
+        <p>${laterForecast.temperature}°F Wind: ${laterForecast.windSpeed}</p>
+        <p>${laterForecast.shortForecast}</p>
+        <p>${laterForecast.detailedForecast}</p>
     `;
   })
   .catch(error => {
