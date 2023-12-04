8edcd31394b0a48edcb5d7cf9b308f8f417ff65b
Julia Bodian <julia.h.bodian@gmail.com>
2023-12-04 16:34:32 -0500

    display some data

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ fetch(URL)
   .then(response => response.json())
   .then(data => {
     console.log(data.properties);
+    let forecast = data.properties.periods[0];
+    const { name, temperature, windSpeed, windDirection, shortForecast, detailedForecast } = forecast;
+    const weather = document.querySelector('#forecast');
+    weather.innerHTML = `
+      <h3>${name}</h3>
+      <p>${temperature} ${windSpeed} ${windDirection}</p>
+      <p>${shortForecast}</p>
+      <p>${detailedForecast}</p>
+    `;
   })
   .catch(error => {
     console.error('Error:', error);
